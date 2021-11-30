653b7f08ae423c9b39fb21495bb5325d6e54ecb1
Muhammad Ibrahim <iby.khan7@gmail.com>
2021-11-30 19:49:39 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Route, Routes} from "react-router";
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <Routes>
+      <Route path ='/' element={<Login />} />
+      <Route path ='/Signup' element={<Signup />} />
+      <Route path ='/Home' element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
